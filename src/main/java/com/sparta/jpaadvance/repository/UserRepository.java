30b2cf5f925448aa8d03852a5ce981de584da102
sanghoon1109 <tkdgns7108@email.com>
2023-11-30 00:43:28 +0900

    @OneToOne 단방향 테스트

---
+++ src/main/java/com/sparta/jpaadvance/repository/UserRepository.java
@@ -0,0 +1,9 @@
+package com.sparta.jpaadvance.repository;
+
+import com.sparta.jpaadvance.entity.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.transaction.annotation.Transactional;
+
+
+public interface UserRepository extends JpaRepository<User, Long> {
+}
