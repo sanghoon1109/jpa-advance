3f7130da98281e0704455c8675d3153010714533
sanghoon1109 <tkdgns7108@email.com>
2023-12-01 17:52:12 +0900

    @ManyToMany 양방향 및 조회 테스트

--- src/main/java/com/sparta/jpaadvance/entity/User.java
+++ src/main/java/com/sparta/jpaadvance/entity/User.java
@@ -4,6 +4,9 @@ import jakarta.persistence.*;
 import lombok.Getter;
 import lombok.Setter;
 
+import java.util.ArrayList;
+import java.util.List;
+
 @Entity
 @Getter
 @Setter
@@ -14,5 +17,11 @@ public class User {
     private Long id;
     private String name;
 
+    @ManyToMany(mappedBy = "userList")
+    private List<Food> foodList = new ArrayList<>();
 
+    public void addFoodList(Food food) {
+        this.foodList.add(food);
+        food.getUserList().add(this); // 외래 키(연관 관계) 설정
+    }
 }
